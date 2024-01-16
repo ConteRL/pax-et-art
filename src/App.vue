83efcc8a8d06db871116f28846e9a3f2f91845b9
ConteRL <rodrigo.oliveira2003@outlook.com>
2024-01-15 21:49:45 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <h1>Pax et Art</h1>
+
+    <div>
+      <ul>
+        <li>Inicio</li>
+        <li>Orações</li>
+        <li>Santos</li>
+        <li>Filmes</li>
+        <li>Sobre</li>
+      </ul>
+    </div>
+
+    <div>
+      <img src="./assets/github.svg" alt="Logo Github">
+      <span>/ConteRL</span>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
